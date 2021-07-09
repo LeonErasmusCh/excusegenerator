f746234455773e6eda83ed34e0247fdbd6a81598
LeonErasmusCh <leon@santiagobiltong.cl>
2021-07-09 01:36:00 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function excuse() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  /*
+   */
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let random_who = Math.floor(Math.random() * who.length);
+
+  let what = ["my homework", "the keys", "the car"];
+  let random_what = Math.floor(Math.random() * what.length);
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  let random_action = Math.floor(Math.random() * action.length);
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let random_when = Math.floor(Math.random() * when.length);
+  /*console.log(
+    who[random_who],
+    action[random_action],
+    what[random_what],
+    when[random_when]
+  );
+  */
+  let result =
+    who[random_who] +
+    " " +
+    action[random_action] +
+    " " +
+    what[random_what] +
+    " " +
+    when[random_when];
+  document.getElementById("excuse").innerHTML = result;
+}
+
+document.getElementById("btn").addEventListener("click", excuse);
